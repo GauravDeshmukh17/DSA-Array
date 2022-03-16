3f53e242da23ac275accc5507e38c9a6550ce356
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-03-16 21:14:45 +0530

    first program of array

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int arr[]=new int[5];
+
+        arr[0]=12;
+        arr[1]=23;
+        arr[2]=45;
+        arr[3]=10;
+        arr[4]=-15;
+
+        for(int i=0;i<5;i++){
+
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
