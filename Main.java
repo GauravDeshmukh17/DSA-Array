096472f6e547d883c1ccf4e1b38964c94e0fb3bc
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-03-16 22:01:25 +0530

    array program

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-package com.company;
+/*package com.company;
 
 public class Main {
 
@@ -17,4 +17,39 @@ public class Main {
             System.out.print(arr[i]+" ");
         }
     }
-}
+}*/
+
+//-------------------------------------------------------------------------------------------------
+
+/*package com.company;
+
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the size of Array");
+        int n=scn.nextInt();
+
+        int arr[]=new int[n];
+
+        System.out.println("Enter the elements of Array");
+        for(int i=0;i<n;i++){
+
+            arr[i]=scn.nextInt();
+        }
+
+        System.out.println("Elements of Array are :");
+        for(int i=0;i<n;i++){
+
+            System.out.print(arr[i]+"  ");
+        }
+
+    }
+
+}*/
+
+//--------------------------------------------------------------------------------------------
+
