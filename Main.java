a3bb7ec862aba14cf5d80f238581a6cdeca0e78d
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-07-09 18:45:32 +0530

    state of wakanda

--- Main.java
+++ Main.java
@@ -926,7 +926,7 @@ public class Main{
 
 // ARRAY TRANSPOSE
 
-package com.company;
+/*package com.company;
 
 import java.util.*;
 
@@ -968,7 +968,50 @@ public class Main{
         originalArray(arr,m,n);
         int at[][]=arrayTranspose(arr,m,n);
     }
-}
+}*/
 
+//-------------------------------------------------------------------------------------------------------
+
+// STATE OF WAKANDA-I
+
+package com.company;
+
+import java.util.*;
+
+public class Main{
+
+    public static int[][] stateOfWakanda(int arr[][],int m,int n){
+
+        for(int j=0;j<n;j++){
+            if(j%2==0){
+                for(int i=0;i<m;i++){
+                    System.out.println(arr[i][j]);
+                }
+
+            }
+            else{
+                for(int i=m-1;i>=0;i--){
+                    System.out.println(arr[i][j]);
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        int m=scn.nextInt();
+        int n=scn.nextInt();
+        int arr[][]=new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+        int sow[][]=stateOfWakanda(arr,m,n);
+    }
+}
 
 
